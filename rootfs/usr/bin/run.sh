721ccd44e292a330096af03b8c8bfa6e71f7e43b
Ahmed Waleed <41992504+Ahmed9190@users.noreply.github.com>
2026-02-13 14:16:15 +0100

    Use custom Python server

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
-cd /usr/local/share/hello
-python3 -m http.server 1880
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class HelloHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/' or self.path == '/index.html':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'<html><body><h1>Hello World!</h1></body></html>')
+        else:
+            super().do_GET()
+
+HTTPServer(('0.0.0.0', 1880), HelloHandler).serve_forever()
