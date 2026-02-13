0355c7134355b2182c93bfa8d1a4d07e89f3abc5
Ahmed Waleed <41992504+Ahmed9190@users.noreply.github.com>
2026-02-13 14:17:44 +0100

    Add FastAPI server

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 ARG BUILD_FROM
 FROM $BUILD_FROM
 
-RUN apk add --no-cache python3
+RUN apk add --no-cache python3 py3-pip
 
 COPY rootfs /
+
+RUN pip3 install --no-cache-dir -r /data/requirements.txt
 
 RUN chmod a+x /usr/bin/run.sh
 
--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-class HelloHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/' or self.path == '/index.html':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'<html><body><h1>Hello World!</h1></body></html>')
-        else:
-            super().do_GET()
-
-HTTPServer(('0.0.0.0', 1880), HelloHandler).serve_forever()
+#!/bin/bash
+cd /data
+uvicorn main:app --host 0.0.0.0 --port 1880
